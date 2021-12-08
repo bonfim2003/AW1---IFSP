d88750f539aeb5b1f1f72617626173efeae8dbfa
bonfim2003 <78187184+bonfim2003@users.noreply.github.com>
2021-12-07 23:40:58 -0300

    Add files via upload

---
+++ Atividades/A20/ex2/main.js
@@ -0,0 +1,25 @@
+const inputCep = document.querySelector('[name=cep]')
+const inputCity = document.querySelector('[name=city]')
+const inputLogr = document.querySelector('[name=logradouro]')
+const inputBairro = document.querySelector('[name=bairro]')
+const inputUF = document.querySelector('[name=uf]')
+const inputDdd = document.querySelector('[name=ddd]')
+
+inputCep.addEventListener('blur', event => {
+  let cep = event.target.value
+
+  //Criar uma promise para receber os dados
+  axios
+    .get(`https://viacep.com.br/ws/${cep}/json/`)
+
+    //parametro resposta recebe
+
+    .then(response => {
+      inputCity.value = response.data.localidade
+      inputUF.value = response.data.uf
+      inputLogr.value = response.data.logradouro
+      inputBairro.value = response.data.bairro
+      inputDdd.value = response.data.ddd
+    })
+    .catch(error => console.log(error))
+})
